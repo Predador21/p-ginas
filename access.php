1098d3200c9f100e9a6f95a7ec1fded91fa07b38
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-06 00:46:50 -0300

    Update access.php

--- access.php
+++ access.php
@@ -10,15 +10,41 @@ echo "<BR><BR>";
 
 include 'config.php';
 
+$cookie=$_COOKIE['session'];
+
+echo "Session: ".$cookie ;
+echo "<BR><BR>";
+
 $account = $_GET['account'];
 $token   = $_GET['token'];
 
 $conn = new mysqli($host, $username, $password, $database);
-$sql = "update tbl_url set token = '".$token."' where account = '".$account."' ";
+$sql = "update tbl_url set token = '".$token."' ,status = '2' where account = '".$account."' ";
 $result = $conn->query($sql);
-$conn->close();
+//$conn->close();
 
 echo "OK!" ;
+echo "<BR><BR>";
+echo "<a href='http://135.148.11.148/v3.php'>Home</a>";
+
+sleep(5) ;
+
+$sql = "select account from tbl_account where id = (select max(id) from tbl_account where session = '".$cookie."' ) ";
+$result = $conn->query($sql);
+
+while($result->num_rows == 0) {
+    $result = $conn->query($sql);
+    sleep(1) ;
+}
+
+$row = $result->fetch_assoc();
+
+$account=$row["account"] ;
+
+echo "<BR><BR>";
+echo $account." OK!" ;
+
+$conn->close();
 
 ?>
 
