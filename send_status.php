c5ee51d3e1470e78cfc54f730e048db326722e5e
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-19 18:00:42 -0300

    Update send_status.php

--- send_status.php
+++ send_status.php
@@ -4,37 +4,18 @@ date_default_timezone_set('America/Sao_Paulo');
 
 include 'config.php';
 
-$refresh     = $_GET['refresh'];
-$status_new  = $_GET['status'];
-$owner       = $_GET['owner'];
+$refresh = $_GET['refresh'];
+$status  = $_GET['status'];
+$owner   = $_GET['owner'];
 
-$data_hora = date('d/m/Y H:i:s');
+//$data_hora = date('d/m/Y H:i:s');
 
 $conn = new mysqli($host, $username, $password, $database);
 
-$sql  ="select status " ;
-$sql .="      ,d_status_first " ;
-$sql .="  from tbl_account " ;
-$sql .=" where refresh_token = '".$refresh."' " ;
-
-$result = $conn->query($sql);
-$row    = $result->fetch_assoc();
-
-$status_old     =$row["status"] ;
-$d_status_first =$row["d_status_first"] ;
-
-if ($status_old != $status_new && $status_new != 'STARTING') {
-    $status=$status_new ;
-    $d_status_first=$data_hora ;
-} else {
-       $status=$status_old ;
-}
-
 $sql  ="update tbl_account " ;
-$sql .="   set status = '".$status."' " ;
-$sql .="      ,d_status_first = '".$d_status_first."' " ;
-$sql .="      ,d_status_last = '".$data_hora."' ";
-$sql .="      ,owner = '".$owner."' ";
+$sql .="   set status   = '".$status."' " ;
+$sql .="      ,d_status = now()";
+$sql .="      ,owner    = '".$owner."' ";
 $sql .=" where refresh_token = '".$refresh."' " ;
 
 $result = $conn->query($sql);
