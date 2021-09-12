bbcaa0da6770730382633871e03856c947eb9a9d
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-11 21:31:22 -0300

    Update send_status.php

--- send_status.php
+++ send_status.php
@@ -7,6 +7,12 @@ include 'config.php';
 $refresh = $_GET['refresh'];
 $status  = $_GET['status'];
 $owner   = $_GET['owner'];
+$bearer  = $_GET['bearer'];
+
+if ( $status == '' )
+{
+ $status = 'PHP' ;
+}
 
 $conn = new mysqli($host, $username, $password, $database);
 
@@ -14,6 +20,7 @@ $sql  ="update tbl_account                      " ;
 $sql .="   set status         = '".$status."'   " ;
 $sql .="      ,d_status       = now()           " ;
 $sql .="      ,owner          = '".$owner."'    " ;
+$sql .="      ,bearer         = '".$bearer."'   " ;
 $sql .=" where refresh_token  = '".$refresh."'  " ;
 
 $result = $conn->query($sql);
