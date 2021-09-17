ad253bc23e11231da7f2b7f170dd9a960b2e42b0
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 17:49:48 -0300

    Create session.php

---
+++ session.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'config.php';
+
+$session = $_GET['session'];
+$account = $_GET['account'];
+$status  = $_GET['status'];
+$url     = $_GET['url'];
+
+$conn = new mysqli($host, $username, $password, $database);
+
+$sql = "insert into tbl_url (session,account,status,url) values ('".$session."' , '".$account."' , '".$status."' , '".$url."' )" ;
+
+$result = $conn->query($sql);
+
+$conn->close();
+
+?>
