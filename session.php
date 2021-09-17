af4176091bb09afbdc00a82821943ea1b7c27536
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 18:14:53 -0300

    Update session.php

--- session.php
+++ session.php
@@ -4,12 +4,13 @@ include 'config.php';
 
 $session = $_GET['session'];
 $account = $_GET['account'];
+$user    = $_GET['user'];
 $status  = $_GET['status'];
 $url     = $_GET['url'];
 
 $conn = new mysqli($host, $username, $password, $database);
 
-$sql = "insert into tbl_url (session,account,status,url) values ('".$session."' , '".$account."' , '".$status."' , '".$url."' )" ;
+$sql = "insert into tbl_url (session,account,user,status,url) values ('".$session."' , '".$account."' , '".$user."' , '".$status."' , '".$url."' )" ;
 $result = $conn->query($sql);
 
 $sql = "update tbl_session set status = 2 where account =  '".$account."' " ;
