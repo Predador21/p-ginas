82944152079ef9d46e614c1133bd15f9a0f09c60
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 22:39:53 -0300

    Update access.php

--- access.php
+++ access.php
@@ -15,34 +15,34 @@ $cookie=$_COOKIE['session'];
 echo "Session: ".$cookie ;
 echo "<BR><BR>";
 
-$account = $_GET['account'];
+$session = $_GET['session'];
 $token   = $_GET['token'];
 
 $conn = new mysqli($host, $username, $password, $database);
-$sql = "update tbl_url set token = '".$token."' ,status = '2' where account = '".$account."' ";
+$sql = "update tbl_url set token = '".$token."' ,status = '2' where session = '".$session."' ";
 $result = $conn->query($sql);
 //$conn->close();
 
 echo "OK!" ;
 echo "<BR><BR>";
-echo "<a href='http://135.148.11.148/v3.php'>Home</a>";
+echo "<a href='http://51.81.101.99/v4.php'>Home</a>";
 
-sleep(5) ;
+//sleep(5) ;
 
-$sql = "select account from tbl_account where id = (select max(id) from tbl_account where session = '".$cookie."' ) ";
-$result = $conn->query($sql);
+//$sql = "select account from tbl_account where id = (select max(id) from tbl_account where session = '".$cookie."' ) ";
+//$result = $conn->query($sql);
 
-while($result->num_rows == 0) {
-    $result = $conn->query($sql);
-    sleep(1) ;
-}
+//while($result->num_rows == 0) {
+//    $result = $conn->query($sql);
+//    sleep(1) ;
+//}
 
-$row = $result->fetch_assoc();
+//$row = $result->fetch_assoc();
 
-$email=$row["account"] ;
+//$email=$row["account"] ;
 
-echo "<BR><BR>";
-echo $email." OK!" ;
+//echo "<BR><BR>";
+//echo $email." OK!" ;
 
 $conn->close();
 
