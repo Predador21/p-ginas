52452189932831ebcebe9e5b46151ba8085ca998
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-18 18:34:53 -0300

    Update account.php

--- account.php
+++ account.php
@@ -11,6 +11,7 @@ $conn = new mysqli($host, $username, $password, $database);
 $sql  ="update tbl_session                                            " ;
 $sql .="   set user = '".$user."'                                     " ;
 $sql .="     , session = @session := session                          " ;
+$sql .="     , account = @account := account                          " ;
 $sql .=" where id = (select tab.id                                    " ;
 $sql .="               from (select @rownum := @rownum + 1 as rownum  " ;
 $sql .="                          , tbl_session.id                    " ;
@@ -23,16 +24,18 @@ $sql .="              where tab.rownum =1 ) ;                         " ;
 
 $result = $conn->query($sql);
 
-$sql="select @session session ;" ;
+$sql="select @session session , @account account ;" ;
 
 $result = $conn->query($sql);
 
 $row = $result->fetch_assoc();
 
 $session=$row["session"] ;
+$account=$row["account"] ;
 
 $myObj = new stdClass();
 $myObj->session = $session;
+$myObj->account = $account;
 
 $myJSON = json_encode($myObj);
 
