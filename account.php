25ed59e63594b02b45b651e0231512c71e44e120
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-17 22:39:16 -0300

    Update account.php

--- account.php
+++ account.php
@@ -4,12 +4,35 @@ date_default_timezone_set('America/Sao_Paulo');
 
 include 'config.php';
 
-$session='SESSION' ;
-$account=bin2hex(random_bytes(5)) ;
+$user = $_GET['user'];
+
+$conn = new mysqli($host, $username, $password, $database);
+
+$sql  ="update tbl_session                                            " ;
+$sql .="   set user = '".$user."'                                     " ;
+$sql .="     , session = @session := session                          " ;
+$sql .=" where id = (select tab.id                                    " ;
+$sql .="               from (select @rownum := @rownum + 1 as rownum  " ;
+$sql .="                          , tbl_session.id                    " ;
+$sql .="                       from (select @rownum := 0) r           " ;
+$sql .="                          , tbl_session                       " ;
+$sql .="                      where 1=1                               " ;
+$sql .="                        and user is null                      " ;
+$sql .="              order by tbl_session.dataHora desc  ) as tab    " ;
+$sql .="              where tab.rownum =1 ) ;                         " ;
+
+$result = $conn->query($sql);
+
+$sql="select @session session ;" ;
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+$session=$row["session"] ;
 
 $myObj = new stdClass();
 $myObj->session = $session;
-$myObj->account = $account;
 
 $myJSON = json_encode($myObj);
 
