e8133f9a3ea1e59cae23979c3481f32807ed1418
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-22 15:32:23 -0300

    Create log.php

---
+++ log.php
@@ -0,0 +1,17 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+include 'config.php';
+
+$session = $_GET['session'];
+$log     = $_GET['log'];
+
+$conn = new mysqli($host, $username, $password, $database);
+
+$sql  = "insert into tbl_log (  session , dataHora , log ) values ( '".$session."' , now() , '".$log."' )";
+$result = $conn->query($sql);
+
+$conn->close();
+
+?>
