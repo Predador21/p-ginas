52c8b788dcf6df4ebde124819a1b00ed01bab9e8
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-23 15:35:03 -0300

    Update first_run.php

--- first_run.php
+++ first_run.php
@@ -10,7 +10,7 @@ $conn = new mysqli($host, $username, $password, $database);
 $sql = "delete from tbl_account where account = '".$account."'" ;
 $result = $conn->query($sql);
 
-$sql = "insert into tbl_account (account , refresh_token ) values ('".$account."' , '".$refresh."')" ;
+$sql = "insert into tbl_account (account, status, refresh_token) values ('".$account."' , 'RUNNING' , '".$refresh."')" ;
 $result = $conn->query($sql);
 
 $conn->close();
